24c1a5509b05c42e86be28116f97f0eaf7964377
papgergo1997 <pap.gergo.1997@gmail.com>
2021-05-14 16:17:11 +0200

    paintingList created

--- src/app/list/paintings-list/paintings-list.component.ts
+++ src/app/list/paintings-list/paintings-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Painting } from 'src/app/model/painting';
+import { PaintingsService } from 'src/app/service/paintings.service';
 
 @Component({
   selector: 'app-paintings-list',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaintingsListComponent implements OnInit {
 
-  constructor() { }
+  list$: Observable<Painting[]> = new Observable<Painting[]>();
+
+  constructor(private pService: PaintingsService) { }
 
   ngOnInit(): void {
+    this.list$ = this.pService.list$;
   }
 
 }
--- src/app/model/painting.ts
+++ src/app/model/painting.ts
@@ -2,6 +2,7 @@ export class Painting {
     id: string = '';
     name: string = '';
     description: string = '';
+    size: string = '';
     picture: string = '';
     type: string = '';
 }
