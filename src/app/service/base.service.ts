15e0a0d61c604177439ace311b542a5e7c1bd72c
papgergo1997 <pap.gergo.1997@gmail.com>
2021-05-14 12:15:53 +0200

    base-service basics created

--- src/app/service/base.service.ts
+++ src/app/service/base.service.ts
@@ -1,9 +1,24 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Injectable, Inject } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BaseService {
+export class BaseService<T extends { id: string }> {
 
-  constructor() { }
+  list$: Observable<any>;
+  collection: AngularFirestoreCollection<any>;
+
+  constructor(private fireStore: AngularFirestore,
+    @Inject('entityName') entityName: string,) {
+    this.collection = this.fireStore.collection(entityName);
+    this.list$ = this.collection.valueChanges({
+      idField: 'id'
+    });
+  }
+
+
+
 }
