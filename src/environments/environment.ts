a3c3451045352b75d5690d3cdac29e643bf327b2
papgergo1997 <pap.gergo.1997@gmail.com>
2021-05-14 12:02:26 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBm9OkQYc7A0BMm2BCpD88aDZG4nLM8F8I",
+    authDomain: "mpage-project.firebaseapp.com",
+    projectId: "mpage-project",
+    storageBucket: "mpage-project.appspot.com",
+    messagingSenderId: "444606152641",
+    appId: "1:444606152641:web:3a8b632c1097dae7d0b940",
+    measurementId: "G-NM2MQ520NJ"
+  }
 };
 
 /*
