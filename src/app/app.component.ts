20e5db04ecd493d744ac441c20bace86535b857e
papgergo1997 <pap.gergo.1997@gmail.com>
2021-05-14 12:40:03 +0200

    services are tested and work fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaintingsService } from './service/paintings.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mpage-project';
+  items$: Observable<any>
+  constructor(
+    private pService: PaintingsService
+  ) {
+    this.items$ = this.pService.list$;
+  }
+
 }
