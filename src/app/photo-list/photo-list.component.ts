0539c58f10482e7a27698f457de9ddb15fb3e5fe
papgergo1997 <pap.gergo.1997@gmail.com>
2021-05-16 11:00:32 +0200

    photo server problems but I almost got it

--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { PhotoUploadService } from '../service/photo-upload.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-list.component.scss']
 })
 export class PhotoListComponent implements OnInit {
+  photos: any[];
 
-  constructor() { }
+  constructor(private pUService: PhotoUploadService) { }
 
   ngOnInit(): void {
-  }
+    this.pUService.getFiles(6).snapshotChanges().pipe(
+      map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() })))
+    ).subscribe(photos => this.photos = photos)
+  };
 
 }
