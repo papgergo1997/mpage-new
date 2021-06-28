80fbfaa6e2d134ced0eebd87c2e1ffbc9cc5ee18
papgergo1997 <pap.gergo.1997@gmail.com>
2021-06-28 11:34:26 +0200

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCY1p2jRtN6YboyfNDY-U3JMGNsQhHwKFQ",
+    authDomain: "moms-page-45979.firebaseapp.com",
+    databaseURL: "https://moms-page-45979-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "moms-page-45979",
+    storageBucket: "moms-page-45979.appspot.com",
+    messagingSenderId: "585678334856",
+    appId: "1:585678334856:web:9beaca01e32afc07efa83d",
+    measurementId: "G-V88N93WCCJ"
+  }
 };
