9ce2ee1cb40e14683e4edb1f6f43e6e9e4aef2a8
papgergo1997 <pap.gergo.1997@gmail.com>
2021-09-06 18:01:28 +0200

    restructuring: admin-nav added to admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
 import { AdminComponent } from './pages/admin/admin.component';
 import { SideNavComponent } from './pageComponents/side-nav/side-nav.component';
-import { AdminNavComponent } from './pageComponents/admin-nav/admin-nav.component';
+import { AdminNavComponent } from './pages/admin/admin-nav/admin-nav.component';
 import { AdminFooterComponent } from './pages/admin/admin-footer/admin-footer.component';
 import { ToastrModule } from 'ngx-toastr';
 import { FilterPipe } from './pipe/filter.pipe';
