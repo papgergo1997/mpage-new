1731422c94a5fd915477be4257eb68cef880fc6f
papgergo1997 <pap.gergo.1997@gmail.com>
2021-09-06 18:04:21 +0200

    restructuring: article-card added to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { SliderPipe } from './pipe/slider.pipe';
 import { ArticlesListComponent } from './pages/admin/articles-list/articles-list.component';
 import { ArticleEditComponent } from './pages/admin/article-edit/article-edit.component';
 import { ArticleFilterPipe } from './pipe/article-filter.pipe';
-import { ArticleCardComponent } from './pageComponents/article-card/article-card.component';
+import { ArticleCardComponent } from './pages/home/article-card/article-card.component';
 import { ArticleCardContainerComponent } from './pageComponents/article-card-container/article-card-container.component';
 import { ImageListComponent } from './pages/admin/image-list/image-list.component';
 import { ImageEditComponent } from './pages/admin/image-edit/image-edit.component';
