db7d28df8b78c150e8d6578e5ff32352c19f6f37
papgergo1997 <pap.gergo.1997@gmail.com>
2021-09-07 11:35:40 +0200

    filter pipe modifications

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(arr: any[], key: string): any[] | null {
+  transform(arr: any[] | null, key: string): any[] | null {
     return arr.filter(item =>
       item.name.toLowerCase().includes(key.toLowerCase())
       || item.type.toLowerCase().includes(key.toLowerCase()));
